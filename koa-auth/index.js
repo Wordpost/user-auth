9b8b9c0323fadf4be681bbfd854783ebbef8da0e
weban <wordpost@ya.ru>
2017-12-14 18:35:37 +0400

    fix

---
+++ koa-auth/index.js
@@ -0,0 +1,207 @@
+const Koa = require('koa'); // ядро
+const Router = require('koa-router'); // маршрутизация
+const cors = require('koa-cors');
+const bodyParser = require('koa-bodyparser'); // парсер для POST запросов
+const serve = require('koa-static'); // модуль, который отдает статические файлы типа index.html из заданной директории
+const logger = require('koa-logger'); // опциональный модуль для логов сетевых запросов. Полезен при разработке.
+
+const passport = require('koa-passport'); //реализация passport для Koa
+const LocalStrategy = require('passport-local'); //локальная стратегия авторизации
+const JwtStrategy = require('passport-jwt').Strategy; // авторизация через JWT
+const ExtractJwt = require('passport-jwt').ExtractJwt; // авторизация через JWT
+
+const jwtsecret = "mysecretkey"; // ключ для подписи JWT
+const jwt = require('jsonwebtoken'); // аутентификация  по JWT для hhtp
+const socketioJwt = require('socketio-jwt'); // аутентификация  по JWT для socket.io
+
+const socketIO = require('socket.io');
+
+const mongoose = require('mongoose'); // стандартная прослойка для работы с MongoDB
+const crypto = require('crypto'); // модуль node.js для выполнения различных шифровальных операций, в т.ч. для создания хэшей.
+
+const app = new Koa();
+const router = new Router();
+app.use(cors({ origin: '*' }))
+
+app.use(serve('public'));
+app.use(logger());
+app.use(bodyParser());
+
+app.use(passport.initialize()); // сначала passport
+app.use(router.routes()); // потом маршруты
+const server = app.listen(3000);// запускаем сервер на порту 3000
+
+
+
+mongoose.Promise = Promise; // Просим Mongoose использовать стандартные Промисы
+mongoose.set('debug', true);  // Просим Mongoose писать все запросы к базе в консоль. Удобно для отладки кода
+mongoose.connect('mongodb://localhost/test'); // Подключаемся к базе test на локальной машине. Если базы нет, она будет создана автоматически.
+mongoose.connection.on('error', console.error);
+
+//---------Схема и модель пользователя------------------//
+
+const userSchema = new mongoose.Schema({
+  displayName: String,
+  email: {
+    type: String,
+    required: 'Укажите e-mail',
+    unique: 'Такой e-mail уже существует'
+  },
+  passwordHash: String,
+  salt: String,
+}, {
+  timestamps: true
+});
+
+userSchema.virtual('password')
+.set(function (password) {
+  this._plainPassword = password;
+  if (password) {
+    this.salt = crypto.randomBytes(128).toString('base64');
+    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1');
+  } else {
+    this.salt = undefined;
+    this.passwordHash = undefined;
+  }
+})
+
+.get(function () {
+  return this._plainPassword;
+});
+
+userSchema.methods.checkPassword = function (password) {
+  if (!password) return false;
+  if (!this.passwordHash) return false;
+  return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1') == this.passwordHash;
+};
+
+const User = mongoose.model('User', userSchema);
+
+//----------Passport Local Strategy--------------//
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    session: false
+  },
+  function (email, password, done) {
+    User.findOne({email}, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user || !user.checkPassword(password)) {
+        return done(null, false, {message: 'Нет такого пользователя или пароль неверен.'});
+      }
+      return done(null, user);
+    });
+  }
+  )
+);
+
+//----------Passport JWT Strategy--------//
+
+// Ждем JWT в Header
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: jwtsecret
+};
+
+passport.use(new JwtStrategy(jwtOptions, function (payload, done) {
+    User.findById(payload.id, (err, user) => {
+      if (err) {
+        return done(err)
+      }
+      if (user) {
+        done(null, user)
+      } else {
+        done(null, false)
+      }
+    })
+  })
+);
+
+//------------Routing---------------//
+
+//маршрут для создания нового пользователя
+
+router.post('/users', async(ctx, next) => {
+  try {
+    ctx.body = await User.create(ctx.request.body);
+  }
+  catch (err) {
+    ctx.status = 400;
+    ctx.body = err;
+  }
+});
+
+// маршрут для списка пользователей
+router.get('/users', async(ctx, next) => {
+  try {
+    ctx.body = await User.find();
+    console.log(ctx.body)
+  }
+  catch (err) {
+    ctx.body = err
+  }
+})
+
+//маршрут для локальной авторизации и создания JWT при успешной авторизации
+router.post('/login', async(ctx, next) => {
+  await passport.authenticate('local', function (err, user) {
+    if (user == false) {
+      ctx.body = "Login failed";
+    } else {
+      //--payload - информация которую мы храним в токене и можем из него получать
+      const payload = {
+        id: user.id,
+        displayName: user.displayName,
+        email: user.email
+      };
+      const token = jwt.sign(payload, jwtsecret); //здесь создается JWT
+
+      ctx.body = {
+        id: user.id,
+        email: user.email,
+        user: user.displayName,
+        token: 'JWT ' + token
+      };
+    }
+  })(ctx, next);
+
+});
+
+// маршрут для авторизации по токену
+
+router.get('/user', async(ctx, next) => {
+
+  await passport.authenticate('jwt', function (err, user) {
+    if (user) {
+      ctx.body = {
+        id: user.id,
+        email: user.email,
+        user: user.displayName
+      };
+    } else {
+      ctx.body = "No such user";
+      console.log("err", err)
+    }
+  } )(ctx, next)
+
+});
+
+//---Socket Communication-----//
+let io = socketIO(server);
+
+io.on('connection', socketioJwt.authorize({
+  secret: jwtsecret,
+  timeout: 15000
+})).on('authenticated', function (socket) {
+
+  console.log('Это мое имя из токена: ' + socket.decoded_token.displayName);
+
+  socket.on("clientEvent", (data) => {
+    console.log(data);
+  })
+});
